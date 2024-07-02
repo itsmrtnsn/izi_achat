9cc456daf032c9cdeddc879193f313ed8ab53b70
Mortensen Ulysse <mrtnsn2001@gmail.com>
2024-07-02 18:53:58 -0400

    Define the color pallete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 const HomePage = () => {
 	return (
-		<div>
-			<p>this is a test to see if the geist font is applied</p>
+		<div className='m-4 space-y-5'>
+			<p className='border p-1 text-ring  bg-card'>
+				this is a test to see if the geist font is applied
+			</p>
+
+			<Input placeholder='input' className='transition-all' />
 		</div>
 	);
 };
